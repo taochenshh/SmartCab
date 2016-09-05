b0552d42aa929676a9a7042d792693070bad7dfd
CTTC <chentao904@163.com>
2016-09-05 21:18:00 +0800

    Third Commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Project 4: Reinforcement Learning
+## Train a Smartcab How to Drive
+This project aims to implement a Q-learning (Epsilon-greedy) algorithm to train a smartcab to drive. 
+### Install
+
+ * Python 2.7
+ * [pygame](https://www.pygame.org/wiki/GettingStarted)
+ * [NumPy](http://www.numpy.org/)
+
+If you do not have Python installed yet, it is highly recommended that you install the Anaconda distribution of Python, which already has the above packages and more included. Make sure that you select the Python 2.7 installer and not the Python 3.x installer. pygame can then be installed using one of the following commands:
+
+Windows & Linux:
+```bash
+conda install -c https://conda.anaconda.org/tlatorre pygame
+```
+
+### Description
+In the not-so-distant future, taxicab companies across the United States no longer employ human drivers to operate their fleet of vehicles. Instead, the taxicabs are operated by self-driving agents — known as smartcabs — to transport people from one location to another within the cities those companies operate. In major metropolitan areas, such as Chicago, New York City, and San Francisco, an increasing number of people have come to rely on smartcabs to get to where they need to go as safely and efficiently as possible. Although smartcabs have become the transport of choice, concerns have arose that a self-driving agent might not be as safe or efficient as human drivers, particularly when considering city traffic lights and other vehicles. To alleviate these concerns, your task as an employee for a national taxicab company is to use reinforcement learning techniques to construct a demonstration of a smartcab operating in real-time to prove that both safety and efficiency can be achieved.
+
+### Environment
+The smartcab operates in an ideal, grid-like city (similar to New York City), with roads going in the North-South and East-West directions. Other vehicles will certainly be present on the road, but there will be no pedestrians to be concerned with. At each intersection there is a traffic light that either allows traffic in the North-South direction or the East-West direction. U.S. Right-of-Way rules apply:
+* On a green light, a left turn is permitted if there is no oncoming traffic making a right turn or coming straight through the intersection.
+* On a red light, a right turn is permitted if no oncoming traffic is approaching from your left through the intersection. 
+
+
+### Inputs and Outputs
+Assume that the smartcab is assigned a route plan based on the passengers’ starting location and destination. The route is split at each intersection into waypoints, and the smartcab, at any instant, is at some intersection in the world. Therefore, the next waypoint to the destination, assuming the destination has not already been reached, is one intersection away in one direction (North, South, East, or West). The smartcab has only an egocentric view of the intersection it is at: It can determine the state of the traffic light for its direction of movement, and whether there is a vehicle at the intersection for each of the oncoming directions. For each action, the smartcab may either idle at the intersection, or drive to the next intersection to the left, right, or ahead of it. Finally, each trip has a time to reach the destination which decreases for each action taken (the passengers want to get there quickly). If the allotted time becomes zero before reaching the destination, the trip has failed.
+
+
+###  Rewards and Goal
+The smartcab receives a reward for each successfully completed trip, and also receives a smaller reward for each action it executes successfully that obeys traffic rules. The smartcab receives a small penalty for any incorrect action, and a larger penalty for any action that violates traffic rules or causes an accident with another vehicle. Based on the rewards and penalties the smartcab receives, the self-driving agent implementation should learn an optimal policy for driving on the city roads while obeying traffic rules, avoiding accidents, and reaching passengers’ destinations in the allotted time.
+
+
+### Run
+
+In a terminal or command window, navigate to the top-level project directory `smartcab/` (that contains this README) and run one of the following commands:
+
+```python smartcab/agent.py```  
+```python -m smartcab.agent```
+
+This will run the `agent.py` file and execute the agent code.
+
+
+### How it looks like
+![GUI](GUI.png)
---
+++ smartcab/agent.py
@@ -0,0 +1,142 @@
+import random
+from environment import Agent, Environment
+from planner import RoutePlanner
+from simulator import Simulator
+import numpy as np
+from collections import namedtuple
+
+
+input_keys = Environment.valid_inputs.keys()
+input_keys.append('next_waypoint')
+states = namedtuple('states',input_keys)
+class LearningAgent(Agent):
+    """An agent that learns to drive in the smartcab world."""
+
+    def __init__(self, env):
+        super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
+        self.color = 'red'  # override color
+        self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+        # TODO: Initialize any additional variables here
+
+        # How likely we are gone explore new paths?
+        self.epsilon = 0.05
+
+        # Q learning update formula:
+        # https://en.wikipedia.org/wiki/Q-learning
+        # Good tutorial to start:
+        # http://mnemstudio.org/path-finding-q-learning-tutorial.htm
+        self.learning_rate = 0.90
+
+        #the initial value of Q value
+        self.default_q = 0
+
+        # discount factor
+        self.gamma = 0.10
+
+        self.Q_values = {}
+        
+        self.prev_state = None
+        self.prev_action = None
+        self.prev_reward = None
+
+        self.penalty_num = 0
+        self.move_num = 0
+ 
+
+    def reset(self, destination=None):
+        self.planner.route_to(destination)
+        # TODO: Prepare for a new trip; reset any variables here, if required
+        self.state = None
+        self.action = None
+        self.reward = None
+
+
+    def update(self, t):
+        # Gather inputs
+        self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
+        inputs = self.env.sense(self)
+        deadline = self.env.get_deadline(self)
+
+        
+        # Update state
+        self.state = states(light = inputs['light'], oncoming = inputs['oncoming'], left = inputs['left'], right = inputs['right'], next_waypoint = self.next_waypoint)
+
+        # Select action according to your policy
+        action, max_q_value = self.get_action(self.state)
+
+        
+
+        # Execute action and get reward
+        reward = self.env.act(self, action)
+
+
+        # update penalty_num and move_num
+        self.move_num += 1
+        if reward < 0:
+        	self.penalty_num += 1
+
+        
+
+        self.save_state(self.state, action, reward)
+
+        # Learn policy based on state, action, reward
+        if self.prev_action != None:
+            self.update_q_values(self.prev_state, self.prev_action, self.prev_reward, max_q_value)
+
+
+        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+
+
+    def save_state(self, state, action, reward):
+    	self.prev_state = self.state
+        self.prev_action = action
+        self.prev_reward = reward
+
+
+    def update_q_values(self, prev_state, prev_action, prev_reward, max_q_value):
+    	old_q_value = self.Q_values.get((prev_state,prev_action), self.default_q)
+    	new_q_value = old_q_value + self.learning_rate * (prev_reward + self.gamma * max_q_value - old_q_value)
+    	self.Q_values[(prev_state,prev_action)] = new_q_value
+
+
+    def get_action(self, state):
+
+    	if random.random() < self.epsilon:
+    		action_selected = random.choice(Environment.valid_actions)
+    		q_value_selected = self.get_q_value(state, action_selected)
+    	else:
+    		q_value_selected = -10000000
+    		for action in Environment.valid_actions:
+    			q_value = self.get_q_value(state, action)
+    			if q_value > q_value_selected:
+    				q_value_selected = q_value
+    				action_selected = action
+    			elif q_value == q_value_selected: #if there are two actions that lead to same q value, we need to randomly choose one between them
+    			    action_selected = random.choice([action_selected, action])
+
+    	return action_selected, q_value_selected
+
+    def get_q_value(self, state, action):
+    	return self.Q_values.get((state,action), self.default_q)
+
+
+def run():
+    """Run the agent for a finite number of trials."""
+
+    # Set up environment and agent
+    e = Environment()  # create environment (also adds some dummy traffic)
+    a = e.create_agent(LearningAgent)  # create agent
+    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
+
+    # Now simulate it
+    sim = Simulator(e, update_delay=0.005, display=True)  # create simulator (uses pygame when display=True, if available)
+    # NOTE: To speed up simulation, reduce update_delay and/or set display=False
+
+    sim.run(n_trials=100)  # run for a specified number of trials
+    # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
+
+
+if __name__ == '__main__':
+    run()
+
